b09128474944a0c843e52b3191dfbcf9fc2eb148
Shlyapin Ilya <shlyapin2005@gmail.com>
2025-12-09 14:42:58 +0300

    hw1(Caches): Add CI workflow

--- hw1_Cache/conanfile.py
+++ hw1_Cache/conanfile.py
@@ -1,20 +1,34 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 
 class MainRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators = "CMakeDeps"
+    
+    options = { 
+        "ecc": [True, False],
+        "btest": [True, False]
+    }
+    
+    default_options = { 
+        "ecc": False,
+        "btest": False
+    }
 
     def requirements(self):
         self.requires("cli11/2.5.0")
         self.requires("gtest/1.16.0")
-        
-    def build_requirements(self):
-        self.tool_requires("cmake/4.0.1")
-        self.tool_requires("ninja/1.13.1")
 
     def layout(self):
         cmake_layout(self, build_folder="build")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        if self.options.ecc:
+            tc.variables["CMAKE_EXPORT_COMPILE_COMMANDS"] = "ON"
+        if self.options.btest:
+            tc.variables["BUILD_TEST"] = "ON"
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
